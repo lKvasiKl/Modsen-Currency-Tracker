900bce2dd5c2c05a5ed8b1ecaab0444db3a51553
Kirill Vasko <kirill.vasko.2003@gmail.com>
2023-10-04 12:18:36 +0300

    Updates

---
+++ 620.index.c6308e7f3c2e3f6f2805.js
@@ -0,0 +1,107 @@
+"use strict";(self.webpackChunkmodsen_currency_tracker=self.webpackChunkmodsen_currency_tracker||[]).push([[620],{620:(e,t,o)=>{o.r(t),o.d(t,{default:()=>w});var n=o(697),r=o.n(n),l=o(394);const i=l.ZP.form`
+  display: flex;
+  flex-direction: column;
+  color: ${({theme:e})=>e.textColor};
+  padding-top: 10px;
+`,s=l.ZP.label`
+  background: transparent;
+  font-size: ${({theme:e})=>e.fontSize.s};
+`,a=l.ZP.div`
+  color: ${({theme:e})=>e.textColor};
+  height: 35px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid ${({theme:e})=>e.colors.darkGray};
+  align-items: center;
+  margin-bottom: 15px;
+`,c=l.ZP.input`
+  color: ${({theme:e})=>e.textColor};
+  background-color: transparent;
+  font-size: ${({theme:e})=>e.fontSize.s};
+  height: 30px;
+  border: none;
+  outline: none;
+`,d=l.ZP.button`
+  background-color: ${({theme:e})=>e.colors.darkGreen};
+  color: ${({theme:e})=>e.textColor};
+  height: 50px;
+  margin: 20px 0;
+  border-radius: 25px;
+  border: none;
+  cursor: pointer;
+  font-size: ${({theme:e})=>e.fontSize.s};
+
+  &:hover {
+    background-color: ${({theme:e})=>e.colors.yellow};
+  }
+`;var h=o(893);const x=({label:e,type:t})=>(0,h.jsxs)(h.Fragment,{children:[(0,h.jsx)(s,{children:e}),(0,h.jsx)(a,{children:(0,h.jsx)(c,{type:t})})]});x.propTypes={label:r().string.isRequired,type:r().string};const m=()=>(0,h.jsxs)(i,{children:[(0,h.jsx)(x,{label:"Full Name"}),(0,h.jsx)(x,{label:"E-mail",type:"Email"}),(0,h.jsx)(x,{label:"Message"}),(0,h.jsx)(d,{children:"Contact Us"})]}),p=l.ZP.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,f=l.ZP.h2`
+  text-align: center;
+  font-size: ${({theme:e})=>e.fontSize.xl};
+  padding: 30px 0;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  background: linear-gradient(
+    90deg,
+    ${({theme:e})=>e.colors.darkGreen} 0.18%,
+    ${({theme:e})=>e.colors.yellow} 49.3%,
+    ${({theme:e})=>e.colors.lightGreen} 99.88%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`,g=l.ZP.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid ${({theme:e})=>e.textColor};
+  max-width: 800px;
+  height: fit-content;
+  margin: 0 20px 170px 20px;
+  padding: 30px;
+  gap: 50px;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+`,u=l.ZP.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    order: -1;
+  }
+`,b=l.ZP.h3`
+  text-align: left;
+  font-size: ${({theme:e})=>e.fontSize.m};
+  padding-top: 10px;
+  padding-bottom: 15px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  color: ${({theme:e})=>e.textColor};
+`,j=l.ZP.div`
+  display: flex;
+  gap: 10px;
+`,y=l.ZP.a`
+  text-align: left;
+  font-size: ${({theme:e})=>e.fontSize.s};
+  font-style: normal;
+  line-height: normal;
+  color: ${({theme:e})=>e.colors.gray};
+  font-weight: 400;
+  text-decoration: none;
+
+  &:hover {
+    color: ${({theme:e})=>e.colors.green};
+  }
+`,$=l.ZP.p`
+  text-align: left;
+  font-size: ${({theme:e})=>e.fontSize.s};
+  font-style: normal;
+  line-height: normal;
+  color: ${({theme:e})=>e.colors.gray};
+`,k=()=>(0,h.jsxs)(p,{children:[(0,h.jsx)(f,{children:"Contact us"}),(0,h.jsxs)(g,{children:[(0,h.jsx)(m,{}),(0,h.jsxs)(u,{children:[(0,h.jsx)(b,{children:"Contact"}),(0,h.jsxs)(j,{children:[(0,h.jsx)($,{children:"Email:"}),(0,h.jsx)(y,{href:"mailto:contact@modsen-software.com",children:"contact@modsen-software.com"})]}),(0,h.jsxs)(j,{children:[(0,h.jsx)($,{children:"Phone:"}),(0,h.jsx)(y,{href:"tel:+48501157180",children:"+48501157180"})]}),(0,h.jsx)(b,{children:"Based in"}),(0,h.jsx)($,{children:"Stefana Okrzei 1a/10 Warsaw, Poland"})]})]})]}),w=()=>(0,h.jsx)(k,{})}}]);
